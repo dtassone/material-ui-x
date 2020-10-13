b86a518c1b841de7261f70a1b7e2d6f3629079fb
damien <damien.tassone@gmail.com>
2020-10-13 17:43:33 +0200

    fix issue with state and cleanup

--- packages/grid/_modules_/grid/hooks/features/index.ts
+++ packages/grid/_modules_/grid/hooks/features/index.ts
@@ -4,3 +4,6 @@ export * from './useColumnResize';
 export * from './pagination/usePagination';
 export * from './useSelection';
 export * from './useSorting';
+export { rowCountSelector } from './rows/rowsSelector';
+export { rowsSelector } from './rows/rowsSelector';
+export { optionsSelector } from '../utils/useOptionsProp';
